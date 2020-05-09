3f680dba2d9239d5a2d9e3e6748f31df5e107c79
Christine Zosche <christine.zosche@gmail.com>
2020-05-09 21:44:29 +0000

    Automatically backed up by Learn

--- lib/cash_register.rb
+++ lib/cash_register.rb
@@ -10,7 +10,7 @@ class CashRegister
   def add_item(title, price, quantity=1)
     total_item_price = price*quantity
     @total+=total_item_price
-    quantity.times @items << title
+    quantity.times {@items << title}
     @item_price = total_item_price
   end
   
